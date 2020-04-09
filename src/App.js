066342aa2fd997c17299ef61ed61535d0cd5a72c
Daryan <daryanka@hotmail.co.uk>
2020-04-09 14:51:32 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Modal from "./Modal";
 
 function App() {
+  const modalRef = React.useRef();
+
+  const openModal = () => {
+    modalRef.current.openModal()
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={openModal}>Open Modal</button>
+      <Modal ref={modalRef}>
+        <h1>Modal Header</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi eligendi esse facere illo in minima nulla quis reiciendis. Eligendi impedit nostrum quam quod reprehenderit, ullam veritatis. Fuga provident quos velit.</span><span>Accusantium ad, alias animi et eum, excepturi explicabo fuga iusto magnam maxime minima molestias nam nemo nostrum pariatur perspiciatis porro quae quibusdam quidem quis repudiandae sed ullam vel, veniam vero.</span><span>Eligendi nulla quasi quibusdam quod saepe suscipit tenetur voluptas voluptate! Accusamus amet, commodi culpa distinctio dolor eveniet expedita hic iure magnam magni mollitia nulla officia quas, reiciendis repellat sapiente, veniam!</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={() => modalRef.current.close()}>
+          Close Modal
+        </button>
+      </Modal>
     </div>
   );
 }
