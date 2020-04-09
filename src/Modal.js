611d55b82673d7faddf12a2f0faf87c02703388e
Daryan <daryanka@hotmail.co.uk>
2020-04-09 14:52:29 +0100

    Added Modal.js

---
+++ src/Modal.js
@@ -0,0 +1,36 @@
+import React, {forwardRef, useImperativeHandle} from "react";
+import ReactDOM from "react-dom";
+
+const Modal = forwardRef((props,ref) => {
+  const [display, setDisplay] = React.useState(true);
+
+  useImperativeHandle(ref, () => {
+    return {
+      openModal: () => open(),
+      close: () => close()
+    }
+  });
+
+  const open = () => {
+    setDisplay(true)
+  };
+
+  const close = () => {
+    setDisplay(false);
+  };
+
+  if (display) {
+    return ReactDOM.createPortal(
+      <div className={"modal-wrapper"}>
+        <div onClick={close} className={"modal-backdrop"} />
+        <div className={"modal-box"}>
+          {props.children}
+        </div>
+      </div>, document.getElementById("modal-root"))
+  }
+
+  return null;
+
+});
+
+export default Modal
